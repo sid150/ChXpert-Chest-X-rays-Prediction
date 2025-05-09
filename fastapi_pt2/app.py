3c33d8555a6b45069d04f8ee935d3146dd41ea6d
Ruchit Jathania <44512765+RuchitJathania@users.noreply.github.com>
2025-05-09 16:16:12 -0400

    Add files via upload

--- fastapi_pt2/app.py
+++ fastapi_pt2/app.py
@@ -8,7 +8,21 @@ import torchvision.transforms as transforms
 from PIL import Image
 import io
 import numpy as np
+import mlflow
+from mlflow.tracking import MlflowClient
 from prometheus_fastapi_instrumentator import Instrumentator
+
+# Change to environment variable later:
+mlflow.set_tracking_uri("http://129.114.26.91:8000/")
+
+client = MlflowClient()
+experiment = client.get_experiment_by_name("chexpert-classifier")
+runs = client.search_runs(experiment_ids=[experiment.experiment_id],
+                          order_by=["metrics.val_accuracy DESC"],
+                          max_results=2)
+best_run = runs[0]  # The first run is the best due to sorting
+best_run_id = best_run.info.run_id
+model_uri = f"runs:/{best_run_id}/model"
 
 
 app = FastAPI(
@@ -32,10 +46,16 @@ class PredictionResponse(BaseModel):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load the CheXpert model
-MODEL_PATH = "demoModel.pt"
-model = torch.jit.load(MODEL_PATH)
-model.to(device)
-model.eval()
+# try:
+#     model = mlflow.pytorch.load_model(model_uri)
+#     print("Model loaded successfully from MLflow.")
+# except Exception as e:
+#     print(f"Failed to load model from MLflow: {e}")
+#     MODEL_PATH = "demoModel.pt"
+#     model = torch.jit.load(MODEL_PATH)
+#     model.to(device)
+#     model.eval()
+model = mlflow.pytorch.load_model(model_uri)
 
 # Define class labels
 classes = np.array([
@@ -87,8 +107,9 @@ def predict_image(request: ImageRequest):
         #
         # return PredictionResponse(predictions=[classes[predicted_class]], probabilities=[confidence])
         with torch.no_grad():
-            output = model(image)
-            probabilities = F.softmax(output, dim=1).squeeze()  # Shape: (num_classes,)
+            logits = model(image.unsqueeze(0))
+            probabilities = torch.sigmoid(logits).squeeze()
+            # probabilities = F.softmax(output, dim=1).squeeze()  # Shape: (num_classes,)
             prob_list = probabilities.tolist()
 
         # Return all class names and their probabilities
